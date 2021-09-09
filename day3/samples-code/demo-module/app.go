e2750edf52d2b11e48a0a9f7d4d7296a02534dc8
binh.trinh <v.binhtd1@vinid.net>
2021-09-09 17:53:19 +0700

    practice using go module

--- day3/samples-code/demo-module/app.go
+++ day3/samples-code/demo-module/app.go
@@ -3,9 +3,18 @@ package main
 import (
 	"demo-module/utils"
 	"fmt"
+
+	"github.com/binhtd/mygomodule/mathutil"
+	mygomoduleV2 "github.com/binhtd/mygomodule/v2"
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(utils.IsPrime(7))
 	fmt.Println(utils.ProperCase("welcome to the our world!"))
+
+	fmt.Println(mathutil.Add(10, 11))
+	fmt.Println(mathutil.Minus(10, 11))
+	fmt.Println(mygomoduleV2.Lower("Toi yeu Viet Nam"))
+	fmt.Println(quote.Go())
 }
--- day3/samples-code/demo-module/go.mod
+++ day3/samples-code/demo-module/go.mod
@@ -2,4 +2,8 @@ module demo-module
 
 go 1.16
 
-require rsc.io/quote v1.5.2 // indirect
+require (
+	github.com/binhtd/mygomodule/mathutil v0.0.0-20210909084935-be9a1791225b // indirect
+	github.com/binhtd/mygomodule/v2 v2.0.0-20210909084935-be9a1791225b // indirect
+	rsc.io/quote v1.5.2 // indirect
+)
